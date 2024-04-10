3d8dc33ecaad9b359c69f2514762f1a8cbb6dce1
NafiurSiddiqui <saejim91@gmail.com>
2024-04-10 14:14:37 +0600

    config: Tailwind prettier-plugin installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,12 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: [
+export default {content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
-    ],
-
-    theme: {
+    ], theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
@@ -61,7 +58,8 @@ export default {
         },
 
 
-    },
+    }, plugins: [
 
-    plugins: [forms],
-};
+        "prettier-plugin-tailwindcss"
+        ]
+    };
