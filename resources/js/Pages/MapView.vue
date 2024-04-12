ee77e5ff2c07d5a21b34c2a53c0905a14e3baed2
NafiurSiddiqui <saejim91@gmail.com>
2024-04-13 01:57:24 +0600

    UI: Mapview component wip

--- resources/js/Pages/MapView.vue
+++ resources/js/Pages/MapView.vue
@@ -1,10 +1,17 @@
 <script setup>
-
 import NavLink from "@/Components/NavLink.vue";
+import {usePage} from "@inertiajs/vue3";
+import {computed} from "vue";
+
+const page = usePage();
+
+const user = computed(() => page.props.auth.user);
+
+console.log(page.props.auth);
 </script>
 
 <template>
-    Map view ma man!
+    <h1>Hello, {{ user.name }}</h1>
 
     <NavLink :href="route('logout')" method="post" as="button">
         Logout
